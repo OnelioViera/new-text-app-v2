b4911b30a6a32c3ffe51f63e113aae8271bf9ef6
Onelio Viera <ojvwebdesign@gmail.com>
2024-06-09 15:01:00 -0600

    refactor: Update page layout and styles
    This commit refactors the page layout and styles in the `app/page.tsx` file. The changes include:
    - Updating the heading text to use the "Tailwind" framework
    - Adding transition and hover effects to the grid items
    - Adding borders and shadows to the grid items in different colors
    - Adding new grid layouts with different background colors and borders
    
    These changes improve the visual appeal and user experience of the page.

--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,36 @@ const MainPage = () => {
   return (
     <>
       <div className='p-10'>
-        <h1 className='text-3xl font-bold'>Hello, World!</h1>
+        <h1 className='text-3xl font-bold'>Hello, Tailwind!</h1>
         <p className='mt-5'>This is a simple example of a React component using next.js.</p>
       </div>
       <div className='grid lg:grid-cols-4 md:grid-cols-2 gap-4'>
-        <div className='text-white bg-pink-500 col-span-2 hover:col-span-4 text-center py-2 px-2 rounded-lg'>01</div>
-        <div className='text-white bg-pink-500 col-span-2 text-center py-2 px-2 rounded-lg'>02</div>
-        <div className='text-white bg-pink-500 col-span-2 text-center py-2 px-2 rounded-lg'>03</div>
-        <div className='text-white bg-pink-500 col-span-2 text-center py-2 px-2 rounded-lg'>04</div>
-        <div className='lg:hidden text-white bg-pink-500 text-center py-2 px-2 rounded-lg'>05</div>
-        <div className='lg:hidden text-white bg-pink-500 text-center py-2 px-2 rounded-lg'>06</div>
-        <div className='lg:hidden md:hidden text-white bg-pink-500 text-center py-2 px-2 rounded-lg'>07</div>
-        <div className='lg:hidden md:hidden text-white bg-pink-500 text-center py-2 px-2 rounded-lg'>08</div>
+        <div className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-80 hover:bg-indigo-500 duration-300 text-white bg-pink-500 col-span-2 hover:col-span-4 text-center py-2 px-2 rounded-lg shadow-md border-2 border-red-700'>01</div>
+        <div className='text-white bg-pink-500 col-span-2 text-center py-2 px-2 rounded-lg shadow-md border-2 border-red-700'>02</div>
+        <div className='text-white bg-pink-500 col-span-2 text-center py-2 px-2 rounded-lg shadow-md border-2 border-red-700'>03</div>
+        <div className='text-white bg-pink-500 col-span-2 text-center py-2 px-2 rounded-lg shadow-md border-2 border-red-700'>04</div>
+        <div className='lg:hidden text-white bg-pink-500 text-center py-2 px-2 rounded-lg shadow-md border-2 border-red-700'>05</div>
+        <div className='lg:hidden text-white bg-pink-500 text-center py-2 px-2 rounded-lg shadow-md border-2 border-red-700'>06</div>
+        <div className='lg:hidden md:hidden text-white bg-pink-500 text-center py-2 px-2 rounded-lg shadow-md border-2 border-red-700'>07</div>
+        <div className='lg:hidden md:hidden text-white bg-pink-500 text-center py-2 px-2 rounded-lg shadow-md border-2 border-red-700'>08</div>
       </div>
+
+      <div className='grid grid-rows-4 grid-flow-col gap-4 mt-6'>
+        <div className='bg-blue-200 text-white font-bold py-2 px-2 rounded-lg text-center shadow-md border-2 border-blue-400'>row 1</div>
+        <div className='bg-blue-200 text-white font-bold py-2 px-2 rounded-lg text-center shadow-md border-2 border-blue-400'>row 2</div>
+        <div className='bg-blue-200 text-white font-bold py-2 px-2 rounded-lg text-center shadow-md border-2 border-blue-400'>row 3</div>
+        <div className='bg-blue-200 text-white font-bold py-2 px-2 rounded-lg text-center shadow-md border-2 border-blue-400'>row 4</div>
+        <div className='bg-blue-200 text-white font-bold py-2 px-2 rounded-lg text-center shadow-md border-2 border-blue-400'>row 5</div>
+        <div className='bg-blue-200 text-white font-bold py-2 px-2 rounded-lg text-center shadow-md border-2 border-blue-400'>row 6</div>
+        <div className='bg-blue-200 text-white font-bold py-2 px-2 rounded-lg text-center shadow-md border-2 border-blue-400'>row 7</div>
+      </div>
+
+      <div className='grid grid-rows-3 grid-flow-col gap-4 mt-6'>
+        <div className='row-span-3 bg-green-400 text-white font-bold text-center py-2 px-2 border-green-600 border-2 shadow-md rounded-md'>01</div>
+        <div className='col-span-2 bg-green-400 text-white font-bold text-center py-2 px-2 border-green-600 border-2 shadow-md rounded-md'>02</div>
+        <div className='row-span-2 col-span-2 bg-green-400 text-white font-bold text-center py-2 px-2 border-green-600 border-2 shadow-md rounded-md'>03</div>
+      </div>
+
     </>
   )
 };
