4c7339f8f07c795cc8fc09b5d6a6453e7b8fc04d
Onelio Viera <ojvwebdesign@gmail.com>
2024-06-09 20:36:54 -0600

    refactor: Update page layout and styles
    This commit updates the page layout and styles in the `app/page.tsx` file. The changes include:
    - Updating the heading text to use the "Tailwind" framework
    - Adding margin to the paragraph element for better spacing
    
    These changes improve the visual appeal and user experience of the page.

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,11 @@ const MainPage = () => {
     <>
       <div>
         <h1 className='text-3xl font-bold'>Hello, Tailwind!</h1>
-        <p className='my-5'>This is a simple example of a React component using next.js.</p>
+        <p className='my-5'>This is a simple example of a React component using Next.js.</p>
       </div>
       <div className='grid lg:grid-cols-4 md:grid-cols-2 gap-4'>
+
+        {/* Next Component */}
         <div className='text-white bg-pink-500 col-span-2 py-6 px-4 rounded-lg shadow-md border-2 border-red-700'>
           <h1 className='text-center text-4xl font-semibold lg:mb-2 md:mb-6'>Next.js</h1>
           <div className='grid text-center justify-items-center lg:p-10'>
@@ -22,7 +24,23 @@ const MainPage = () => {
           </div>
           <p className='lg:text-xl md:text-md text-center pt-4'>Next.js is an open-source web development framework created by the private company <a className='text-blue-700 font-bold' href='https://nextjs.org/' target='blank_page'>Vercel</a> providing React-based web applications with server-side rendering and static website generation.</p>
         </div>
-        <div className='text-white bg-pink-500 col-span-2 text-center py-2 px-2 rounded-lg shadow-md border-2 border-red-700'>02</div>
+
+        {/* React Component */}
+        <div className='text-white bg-pink-500 col-span-2 text-center py-6 px-4 rounded-lg shadow-md border-2 border-red-700'>
+          <h1 className='text-center text-4xl font-semibold lg:mb-2 md:mb-6'>React</h1>
+          <div className='grid text-center justify-items-center lg:p-10'>
+            <Image
+              src='/images/React.png'
+              alt='image'
+              width={300}
+              height={300}
+              className='hidden lg:block md:block'
+            />
+          </div>
+          <p className='lg:text-lg md:text-md text-center pt-4'><a className='text-blue-700 font-bold' href="https://react.dev/" target='blank_page'>React</a> components receive data and return what should appear on the screen. You can pass them new data in response to an interaction, like when the user types into an input. React will then update the screen to match the new data</p>
+        </div>
+
+        {/* Tailwind Components */}
         <div className='text-white bg-pink-500 col-span-2 text-center py-2 px-2 rounded-lg shadow-md border-2 border-red-700'>03</div>
         <div className='text-white bg-pink-500 col-span-2 text-center py-2 px-2 rounded-lg shadow-md border-2 border-red-700'>04</div>
         <div className='lg:hidden text-white bg-pink-500 text-center py-2 px-2 rounded-lg shadow-md border-2 border-red-700'>05</div>
