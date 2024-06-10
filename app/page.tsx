eda87483a2e704224c5c8deca38780441cd4686e
Onelio Viera <ojvwebdesign@gmail.com>
2024-06-10 11:29:22 -0600

    refactor: Update page layout and styles
    This commit updates the page layout and styles in the `app/page.tsx` file. The changes include:
    - Updating the heading text to use the "Next.js, React and Tailwind!" framework
    - Adding margin to the paragraph element for better spacing
    - Updating the button text and link to provide more information about Next.js and React
    
    These changes improve the visual appeal and user experience of the page.

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import Image from 'next/image';
 const MainPage = () => {
   return (
     <>
-      <div>
-        <h1 className='text-3xl font-bold'>Hello, Tailwind!</h1>
-        <p className='my-5'>This is a simple example of a React component using Next.js.</p>
+      <div className='text-center lg:text-left'>
+        <h1 className='text-3xl font-bold'>Next.js, React and Tailwind!</h1>
+        <p className='my-5'>This is a simple example of a React component using Next.js with Tailwind.</p>
       </div>
-      <div className='grid lg:grid-cols-4 md:grid-cols-2 gap-4'>
+      <div className='grid lg:grid-cols-6 md:grid-cols-2 gap-4'>
 
         {/* Next Component */}
         <div className='text-white text-center bg-pink-500 col-span-2 py-6 px-4 rounded-lg shadow-md border-2 border-red-700'>
@@ -23,9 +23,9 @@ const MainPage = () => {
             />
           </div>
           <div className='mt-4'>
-            <button className='font-semibold bg-gray-900 hover:bg-gray-500 py-2 px-6 rounded-full shadow-lg'>INFO</button>
+            <button className='font-semibold bg-gray-900 hover:bg-gray-500 py-2 px-6 rounded-full shadow-lg'><a href='https://nextjs.org/' target='blank_page'>MORE INFO</a></button>
           </div>
-          <p className='lg:text-xl md:text-md text-center pt-4'> <a className='text-blue-700 font-bold' href='https://nextjs.org/' target='blank_page'>Next.js</a> is an open-source web development framework created by the private companyVercel providing React-based web applications with server-side rendering and static website generation.</p>
+          <p className='lg:text-left md:text-left lg:text-xl md:text-md text-center pt-4'> <a className='text-blue-700 font-bold' href='https://nextjs.org/' target='blank_page'>Next.js</a> is an open-source web development framework created by the private companyVercel providing React-based web applications with server-side rendering and static website generation.</p>
         </div>
 
         {/* React Component */}
@@ -41,36 +41,29 @@ const MainPage = () => {
             />
           </div>
           <div className='mt-4'>
-            <button className='font-semibold bg-[#48d2f5] hover:bg-[#3db4d1] py-2 px-6 rounded-full shadow-lg'>INFO</button>
+            <button className='font-semibold bg-[#48d2f5] hover:bg-[#3db4d1] py-2 px-6 rounded-full shadow-lg'><a href='https://react.dev/' target='blank_page'>MORE INFO</a></button>
           </div>
-          <p className='lg:text-xl md:text-md text-center pt-4'><a className='text-blue-700 font-bold' href="https://react.dev/" target='blank_page'>React</a> components receive data and return what should appear on the screen. You can pass them new data in response to an interaction, like when the user types into an input. React will then update the screen to match the new data</p>
+          <p className='lg:text-left md:text-left lg:text-xl md:text-md text-center pt-4'><a className='text-blue-700 font-bold' href="https://react.dev/" target='blank_page'>React</a> components receive data and return what should appear on the screen. You can pass them new data in response to an interaction, like when the user types into an input. React will then update the screen to match the new data</p>
         </div>
 
-        {/* Tailwind Components */}
-        <div className='text-white bg-pink-500 col-span-2 text-center py-2 px-2 rounded-lg shadow-md border-2 border-red-700'>03</div>
-        <div className='text-white bg-pink-500 col-span-2 text-center py-2 px-2 rounded-lg shadow-md border-2 border-red-700'>04</div>
-        <div className='lg:hidden text-white bg-pink-500 text-center py-2 px-2 rounded-lg shadow-md border-2 border-red-700'>05</div>
-        <div className='lg:hidden text-white bg-pink-500 text-center py-2 px-2 rounded-lg shadow-md border-2 border-red-700'>06</div>
-        <div className='lg:hidden md:hidden text-white bg-pink-500 text-center py-2 px-2 rounded-lg shadow-md border-2 border-red-700'>07</div>
-        <div className='lg:hidden md:hidden text-white bg-pink-500 text-center py-2 px-2 rounded-lg shadow-md border-2 border-red-700'>08</div>
-      </div>
-
-      <div className='grid grid-rows-4 grid-flow-col gap-4 mt-6'>
-        <div className='bg-blue-200 text-white font-bold py-2 px-2 rounded-lg text-center shadow-md border-2 border-blue-400'>row 1</div>
-        <div className='bg-blue-200 text-white font-bold py-2 px-2 rounded-lg text-center shadow-md border-2 border-blue-400'>row 2</div>
-        <div className='bg-blue-200 text-white font-bold py-2 px-2 rounded-lg text-center shadow-md border-2 border-blue-400'>row 3</div>
-        <div className='bg-blue-200 text-white font-bold py-2 px-2 rounded-lg text-center shadow-md border-2 border-blue-400'>row 4</div>
-        <div className='bg-blue-200 text-white font-bold py-2 px-2 rounded-lg text-center shadow-md border-2 border-blue-400'>row 5</div>
-        <div className='bg-blue-200 text-white font-bold py-2 px-2 rounded-lg text-center shadow-md border-2 border-blue-400'>row 6</div>
-        <div className='bg-blue-200 text-white font-bold py-2 px-2 rounded-lg text-center shadow-md border-2 border-blue-400'>row 7</div>
-      </div>
-
-      <div className='grid grid-rows-3 grid-flow-col gap-4 mt-6'>
-        <div className='row-span-3 bg-green-400 text-white font-bold text-center py-2 px-2 border-green-600 border-2 shadow-md rounded-md'>01
+        {/* React Component */}
+        <div className='text-white bg-pink-500 col-span-2 text-center py-6 px-4 rounded-lg shadow-md border-2 border-red-700'>
+          <h1 className='text-center text-4xl font-semibold lg:mb-2 md:mb-6'>React</h1>
+          <div className='grid text-center justify-items-center lg:p-10'>
+            <Image
+              src='/images/React.png'
+              alt='image'
+              width={300}
+              height={300}
+              className='hidden lg:block md:block'
+            />
+          </div>
+          <div className='mt-4'>
+            <button className='font-semibold bg-[#48d2f5] hover:bg-[#3db4d1] py-2 px-6 rounded-full shadow-lg'><a href='https://react.dev/' target='blank_page'>MORE INFO</a></button>
+          </div>
+          <p className='lg:text-left md:text-left lg:text-xl md:text-md text-center pt-4'><a className='text-blue-700 font-bold' href="https://react.dev/" target='blank_page'>React</a> components receive data and return what should appear on the screen. You can pass them new data in response to an interaction, like when the user types into an input. React will then update the screen to match the new data</p>
         </div>
-        <div className='col-span-2 bg-green-400 text-white font-bold text-center py-2 px-2 border-green-600 border-2 shadow-md rounded-md'>02</div>
-        <div className='row-span-2 col-span-2 bg-green-400 text-white font-bold text-center py-2 px-2 border-green-600 border-2 shadow-md rounded-md'>03</div>
-      </div>
+        </div>
     </>
   )
 };
