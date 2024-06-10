80ac9f62ff8699bd28d7e61740185a401b765335
Onelio Viera <ojvwebdesign@gmail.com>
2024-06-10 11:37:58 -0600

    refactor: Update page layout and styles
    This commit updates the page layout and styles in the `app/page.tsx` file. The changes include:
    - Updating the heading text to use the "Tailwindcss" framework
    - Adjusting the size and position of the images
    - Updating the button text and link to provide more information about Tailwind CSS
    
    These changes improve the visual appeal and user experience of the page.

--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,8 @@ const MainPage = () => {
             <Image
               src='/images/nextjs.png'
               alt='image'
-              width={300}
-              height={300}
+              width={265}
+              height={265}
               className='hidden lg:block md:block'
             />
           </div>
@@ -46,22 +46,22 @@ const MainPage = () => {
           <p className='lg:text-left md:text-left lg:text-xl md:text-md text-center pt-4'><a className='text-blue-700 font-bold' href="https://react.dev/" target='blank_page'>React</a> components receive data and return what should appear on the screen. You can pass them new data in response to an interaction, like when the user types into an input. React will then update the screen to match the new data</p>
         </div>
 
-        {/* React Component */}
+        {/* Tailwind Component */}
         <div className='text-white bg-pink-500 col-span-2 text-center py-6 px-4 rounded-lg shadow-md border-2 border-red-700'>
-          <h1 className='text-center text-4xl font-semibold lg:mb-2 md:mb-6'>React</h1>
+          <h1 className='text-center text-4xl font-semibold lg:mb-2 md:mb-6'>Tailwindcss</h1>
           <div className='grid text-center justify-items-center lg:p-10'>
             <Image
-              src='/images/React.png'
+              src='/images/tailwind.png'
               alt='image'
-              width={300}
-              height={300}
-              className='hidden lg:block md:block'
+              width={400}
+              height={400}
+              className='hidden lg:block md:block mt-4'
             />
           </div>
           <div className='mt-4'>
             <button className='font-semibold bg-[#48d2f5] hover:bg-[#3db4d1] py-2 px-6 rounded-full shadow-lg'><a href='https://react.dev/' target='blank_page'>MORE INFO</a></button>
           </div>
-          <p className='lg:text-left md:text-left lg:text-xl md:text-md text-center pt-4'><a className='text-blue-700 font-bold' href="https://react.dev/" target='blank_page'>React</a> components receive data and return what should appear on the screen. You can pass them new data in response to an interaction, like when the user types into an input. React will then update the screen to match the new data</p>
+          <p className='lg:text-left md:text-left lg:text-xl md:text-md text-center pt-4'><a className='text-blue-700 font-bold' href="https://tailwindcss.com/" target='blank_page'>Tailwind CSS</a> works by scanning all of your HTML files, JavaScript components, and any other templates for class names, generating the corresponding styles and then writing them to a static CSS file.</p>
         </div>
         </div>
     </>
